9c60f98a89ddf1cfc5ae4b66fbeed6b83dfb2aa2
zakhn <zkhnissi@gmail.com>
2021-03-13 10:56:01 -0500

    ajout des fichiers PWM.cpp PWM.h

---
+++ codeCommun/librairie/PWM.cpp
@@ -0,0 +1,12 @@
+#include "PWM.h"
+
+
+void ajusterPWM(uint8_t duree){
+
+    OCR1A = duree;
+    OCR1B = duree;
+    TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << WGM10);
+    TCCR1B = (1 << CS11);
+    TCCR1C = 0;
+
+}
---
+++ codeCommun/librairie/PWM.h
@@ -0,0 +1,9 @@
+F_CPU 8000000UL
+#include <avr/io>
+
+#ifndef PWM_H
+#define PWM_H
+
+void ajusterPWM(uint8_t duree);
+
+#endif
