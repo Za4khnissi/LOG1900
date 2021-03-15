6303cd26aa64168f55b36cc243cee86eedecf351
zabiullah-sz <zabiullah.shair-zaie@polymtl.ca>
2021-03-15 10:19:37 -0400

    modification de parametre fichier minuteri.cpp

--- codeCommun/tp/tp7/lib_dir/minuterie.h
+++ codeCommun/tp/tp7/lib_dir/minuterie.h
@@ -5,5 +5,5 @@
 namespace timer
 {
     void partirMinuterie(uint16_t duree, uint16_t debut);
-    void initialisationMinuterie (uint8_t valeurDDRA, uint8_t valeurDDRD);
+    void initialisationMinuterie (uint8_t valeurDDRx, uint8_t valeurDDRy, volatile uint8_t* DDRx, volatile uint8_t* DDRy);
 } // namespace name
