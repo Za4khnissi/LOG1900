3167304d6e3247d33d13e22f06086270e20dc999
Moussa <moussa-abdillahi.daoud@polymtl.ca>
2021-04-07 19:09:11 -0400

    lib directory updated

--- projet/lib/Can.cpp
+++ projet/lib/Can.cpp
@@ -17,7 +17,7 @@ Can::Can()
 {
    // ADC mux : reference analogique externe
    //           ajuste a droite
-    ADMUX  = ( 0 << REFS1 ) | ( 0 << REFS0 ) | ( 0 << ADLAR ) | ( 0 << MUX4 ) |
+   ADMUX  = ( 0 << REFS1 ) | ( 0 << REFS0 ) | ( 0 << ADLAR ) | ( 0 << MUX4 ) |
             ( 0 << MUX3 ) | ( 0 << MUX2 ) | ( 0 << MUX1) | ( 0 << MUX0 ) ; 
 
    // ADC Status and Control Register A : 
@@ -26,7 +26,7 @@ Can::Can()
    //  la fin d'une convertion. Division de l'horloge par 64 (il faudra
    //  donc 13 cycles du convertisseur * 64 pour terminer une conversion
    //  soit 832 cycles d'horloge soit 104 micro-secondes
-    ADCSRA = ( 1 << ADEN ) | ( 0 << ADSC )  | ( 0 << ADATE ) | ( 0 << ADIF ) |
+   ADCSRA = ( 1 << ADEN ) | ( 0 << ADSC )  | ( 0 << ADATE ) | ( 0 << ADIF ) |
             ( 0 << ADIE ) | ( 1 << ADPS2 ) | ( 1 << ADPS1 ) | ( 0 << ADPS0 ) ; 
 }
 
@@ -34,38 +34,38 @@ Can::Can()
 Can::~Can()
 {
    // rendre le convertisseur inactif.
-    ADCSRA = 0 << ADEN ;
+   ADCSRA = 0 << ADEN ;
 }
 
 // Faire une conversion et aller retourner le resultat sur 16 bits
 // dont seulement les 10 de poids faibles sont significatifs.
 uint16_t Can::lecture( uint8_t pos)
 {
-    uint16_t adcVal; 
+   uint16_t adcVal; 
 
-    // Garder les bits de ADMUX intacts, sauf les bit permettant 
-    // la selection de l'entree
-    ADMUX  &=  ~(( 1 << MUX4 ) | ( 1 << MUX3 ) | 
-                ( 1 << MUX2 ) | ( 1 << MUX1)  | ( 1 << MUX0 )); 
+   // Garder les bits de ADMUX intacts, sauf les bit permettant 
+   // la selection de l'entree
+   ADMUX  &=  ~(( 1 << MUX4 ) | ( 1 << MUX3 ) | 
+               ( 1 << MUX2 ) | ( 1 << MUX1)  | ( 1 << MUX0 )); 
 
-    // selectionner l'entree voulue
-    ADMUX |= ((pos & 0x07) << MUX0) ;
+   // selectionner l'entree voulue
+   ADMUX |= ((pos & 0x07) << MUX0) ;
 
-    // demarrer la conversion
-    ADCSRA |= ( 1 << ADSC ); 
+   // demarrer la conversion
+   ADCSRA |= ( 1 << ADSC ); 
 
-    // Attendre la fin de la conversion soit 13 cycles du convertisseur.
-    while( ! (ADCSRA & ( 1 << ADIF ) ) );
+   // Attendre la fin de la conversion soit 13 cycles du convertisseur.
+   while( ! (ADCSRA & ( 1 << ADIF ) ) );
 
-    // important: remettre le bit d'indication de fin de cycle a zero 
-    // pour la prochaine conversion ce qui se fait en l'ajustant a un.
-    ADCSRA |= (1 << ADIF);
+   // important: remettre le bit d'indication de fin de cycle a zero 
+   // pour la prochaine conversion ce qui se fait en l'ajustant a un.
+   ADCSRA |= (1 << ADIF);
 
-    // Aller chercher le resultat sur 16 bits.
-    adcVal =   ADCL ;
-    adcVal +=  ADCH << 8;
+   // Aller chercher le resultat sur 16 bits.
+   adcVal =   ADCL ;
+   adcVal +=  ADCH << 8;
 
-    // resultat sur 16 bits
-    return adcVal;
+   // resultat sur 16 bits
+   return adcVal;
 }
 
--- projet/lib/Can.h
+++ projet/lib/Can.h
@@ -30,14 +30,14 @@ class Can
 {
 public:
 
-    Can();
-    ~Can();
+   Can();
+   ~Can();
 
    // retourne la valeur numerique correspondant a la valeur
    // analogique sur le port A.  pos doit etre entre 0 et 7
    // inclusivement.  Seulement les 10 bits de poids faible
    // sont significatifs.
-    uint16_t lecture(uint8_t pos);
+   uint16_t lecture(uint8_t pos);
 
 private:
    // Donnees membres - aucun
