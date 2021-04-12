a1e2663ccc304ef133d1d94353986221e4d7cf0b
zakhn <zkhnissi@gmail.com>
2021-04-12 10:40:17 -0400

    ajout du debounce au boutons

--- projet/exec/main.cpp
+++ projet/exec/main.cpp
@@ -4,120 +4,88 @@
 #include <util/delay.h>
 #include <avr/eeprom.h>
 
-
-/*enum Clavier
-{
-    C1, 
-    C2, 
-    C3,
-    R1, 
-    R2,
-    R3
-};
-
-
-int colonneActive;
-int rangeeActive;
-
-
-void detectionRangee() {
-    DDRC = 0x1C;
-    PORTC = 0x1C;
-    if (PINC & 0x20)
-        rangeeActive = R1;
-    if (PINC & 0x40)
-        rangeeActive = R2;
-    if (PINC & 0x80)
-        rangeeActive = R3;
-
-}
-
-
-void detectionDeBouton() {
-    DDRC = 0xE0;
-
-    PORTC |= 0xE0;
-
-    if (PINC & 0x4){
-        colonneActive = C3;
-        detectionRangee();
-    }
-
-    if (PINC & 0x8){
-        colonneActive = C2;
-        detectionRangee();
-    }
-
-    if (PINC & 0x16){
-        colonneActive = C1;
-        detectionRangee();
-    }
-}
-*/
-
-
+#include "debug.h"
+#include "Button.h"
 
 
 void pressButton(){
-	enum boutonAppuye {
-		one,
-		two,
-		four,
-		r,
-		v,
-		c,
-		i,
-		e,
-		diez
-	};
+    enum boutonAppuye {
+        one,
+        two,
+        four,
+        r,
+        v,
+        c,
+        i,
+        e,
+        diez
+    };
 
-		boutonAppuye bouton;
-		DDRC = 0xE0;
-		PORTC |= _BV(PC7) | _BV(PC6) | _BV(PC5);
-		DDRA = 0x01;
+        boutonAppuye bouton;
+        DDRC = 0xE0;
+        PORTC |= _BV(PC7) | _BV(PC6) | _BV(PC5);
+        DDRA = 0x01;
 
-		if (PINC & 0x04){
-			DDRC = 0x1C;
-			PORTC &= 0x04;
-				if (PINC & 0x80){
-					bouton = diez; // ajouter quoi faire
-				}
-				else if (PINC & 0x40){
-					bouton = c;
-				}
-				else if (PINC & 0x20){
-					bouton = four;
-				}
-	
+        if (PINC & 0x04){
+            DDRC = 0x1C;
+            PORTC = 0x04;
+                if (PINC & 0x80){
+					while(PINC & 0x80){};
+                    DEBUG_PRINT(" # ", 4);
+                    bouton = diez; // ajouter quoi faire
+                }
+                else if (PINC & 0x40){
+					while(PINC & 0x40){};
+                    DEBUG_PRINT(" C ", 4);
+                    bouton = c;
+                }
+                else if (PINC & 0x20){
+					while(PINC & 0x20){};
+                    DEBUG_PRINT(" 4 ", 4);
+                    bouton = four;
+                }
+
+        }
+        if (PINC & 0x08){
+            DDRC = 0x1C;
+            PORTC = 0x08;
+                if (PINC & 0x80){
+					while(PINC & 0x80){};
+                    DEBUG_PRINT(" E ", 4);
+                    PORTA = 0x01; // ajouter quoi faire
+                }
+                else if (PINC & 0x40){
+					while(PINC & 0x40){};
+                    DEBUG_PRINT(" V ", 4);
+                    bouton = v;
+                }
+                else if (PINC & 0x20){
+					while(PINC & 0x20){};
+                    DEBUG_PRINT(" 2 ", 4);
+                    bouton = two;
+                }
+
+        }
+        if (PINC & 0x10){ 
+            DDRC = 0x1C;
+            PORTC = 0x10;
+                if (PINC & 0x80){
+					while(PINC & 0x80){};
+                    DEBUG_PRINT(" I ", 4);
+                    PORTA = 0x00; // ajouter quoi faire
+                }
+                else if (PINC & 0x40){
+					while(PINC & 0x40){};
+                    DEBUG_PRINT(" R ", 4);
+                    bouton = r;
+                }
+                else if (PINC & 0x20){
+					while(PINC & 0x20){};
+                    DEBUG_PRINT(" 1 ", 4);
+                    bouton = one;
+                }
 		}
-		if (PINC & 0x08){
-			DDRC = 0x1C;
-			PORTC &= 0x08;
-				if (PINC & 0x80){
-					PORTA = 0x01; // ajouter quoi faire
-				}
-				else if (PINC & 0x40){
-					bouton = v;
-				}
-				else if (PINC & 0x20){
-					bouton = two;
-				}
-	
-		}
-		if (PINC & 0x10){
-			DDRC = 0x1C;
-			PORTC &= 0x10;
-				if (PINC & 0x80){
-					PORTA = 0x00; // ajouter quoi faire
-				}
-				else if (PINC & 0x40){
-					bouton = r;
-				}
-				else if (PINC & 0x20){
-					bouton = one;
-				}
-	
-		}
+
 }
 
 
