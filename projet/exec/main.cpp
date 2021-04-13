24e9e2c233256a47fef9aa7e8f8d9ae9d3c325a4
zakhn <zkhnissi@gmail.com>
2021-04-13 13:08:59 -0400

    merge du code de can et du clavie

--- projet/exec/main.cpp
+++ projet/exec/main.cpp
@@ -7,6 +7,107 @@
 #include "debug.h"
 #include "Button.h"
 
+#include <stdio.h>
+
+#include "Can.h"
+#include "UART.h"
+
+
+enum Sensor {
+    left,
+    center,
+    right
+};
+
+enum AnalogDigConv {
+    internal,
+    external
+};
+
+void selectSensor(Sensor sensor) {
+    switch (sensor){
+        case left:
+            PORTA &= ~((1 << PA2) | (1 << PA3));
+        break;
+        
+        case center:
+            PORTA |= (1 << PA2);
+            PORTA &= ~(1 << PA3);
+        break;
+        
+        case right:
+            PORTA |= (1 << PA3);
+            PORTA &= ~(1 << PA2);
+        break;
+    }
+}
+
+void selectCan(AnalogDigConv can){
+    switch (can){
+        case internal:
+            PORTA &= ~(1 << PA0);
+        break;
+        
+        case external:
+            PORTA |= (1 << PA0);
+        break;
+    }
+}
+
+uint8_t distance(AnalogDigConv converter) {
+    selectCan(converter);
+
+    if(converter == AnalogDigConv::internal){
+        Can can; 
+        uint8_t volts = can.lecture(PA1);
+        //return can.lecture(PA1) >> 2;
+        return volts; //* (5/1024);
+    }
+    else{
+        uint8_t result = 0;
+
+        PORTB &= ~(1 << PB2) & ~(1 << PB3) & ~(1 << PB4);
+    
+        PORTB |= (1 << PB2) | (1 << PB3) | (1 << PB4); //MUX D7
+        _delay_ms(10);
+        result = PINB & (1<< PB1);          //0000 0001
+
+        PORTB |= (1 << PB3) | (1 << PB4); //MUX D6
+        PORTB &= ~(1 << PB2);
+        _delay_ms(10);
+        result += (PINB & (1<< PB1)) << 1;          //0000 0011
+        
+        PORTB |= (1 << PB2) | (1 << PB4); //MUX D5
+        PORTB &= ~(1 << PB3);
+        _delay_ms(10);
+        result += (PINB & (1<< PB1)) << 2;
+        
+        PORTB |= (1 << PB4); //MUX D4
+        PORTB &= ~((1 << PB2) | (1 << PB3));
+        _delay_ms(10);
+        result += (PINB & (1<< PB1)) << 3;
+        
+        PORTB |= (1 << PB2) | (1 << PB3); //MUX D3
+        PORTB &= ~(1 << PB4);
+        _delay_ms(10);
+        result += (PINB & (1<< PB1)) << 4;
+        
+        PORTB |= (1 << PB3); //MUX D2
+        PORTB &= ~((1 << PB2) | (1 << PB4));
+        _delay_ms(10);
+        result += (PINB & (1<< PB1)) << 5;
+        
+        PORTB |= (1 << PB2); //MUX D1
+        PORTB &= ~((1 << PB3) | (1 << PB4));
+        _delay_ms(10);
+        result += (PINB & (1<< PB1)) << 6;
+        
+        PORTB &= ~((1 << PB2) | (1 << PB3) | (1 << PB4)); //MUX D0
+        _delay_ms(10);
+        result += (PINB & (1<< PB1)) << 7;
+        return result;
+    }
+}
 
 void pressButton(){
     enum boutonAppuye {
@@ -72,7 +173,7 @@ void pressButton(){
                 if (PINC & 0x80){
 					while(PINC & 0x80){};
                     DEBUG_PRINT(" I ", 4);
-                    PORTA = 0x00; // ajouter quoi faire
+                    PORTA = 0x00; 
                 }
                 else if (PINC & 0x40){
 					while(PINC & 0x40){};
